eda381a7e731b637b3fa8c688773a0e07640e593
Skyler Minsu Bang <32551541+skylermbang@users.noreply.github.com>
2021-09-17 22:46:02 +0900

    finish 432 assignment

--- 432/app.js
+++ 432/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 8111;
+const port = 8080;
 const axios = require("axios").default;
 
 const authMiddleware = require("./middlewares/auth-middleware");
